660f53f8fe9c15f35610d5cab2141f2fe75c0248
manoloibanez5 <49238210+manoloibanez5@users.noreply.github.com>
2020-05-05 12:14:57 +0200

    tercer commit

--- templates/cabecera.php
+++ templates/cabecera.php
@@ -10,9 +10,11 @@
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="bg-success">
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-	<a class="navbar-brand" href="index.php">Deportes Albolote</a>
+	<a class="navbar-brand" href="index.php">
+	<img src="https://image.freepik.com/vector-gratis/resumen-silueta-deporte-logo-diseno-plano_23-2148213729.jpg" width="70" height="70" class="d-inline-block align-top" alt="">
+	</a>
 	<button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
